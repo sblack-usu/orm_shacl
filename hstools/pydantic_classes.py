505c21e129d1e49def0409df24a584e02b835477
Scott Black <scott.black@usu.edu>
2020-10-29 17:04:51 -0600

    add rdf_subject and rdf_type to RDFBaseModel

--- hstools/pydantic_classes.py
+++ hstools/pydantic_classes.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from typing import List
 
-from marshmallow import Schema, fields
 from pydantic.fields import Undefined
 from rdflib import Namespace, Graph, BNode, URIRef, Literal
-from pydantic import BaseModel, Field, typing
+from pydantic import BaseModel, Field
 
 HSRESOURCE = Namespace("http://www.hydroshare.org/resource/")
 DCTERMS = Namespace("http://purl.org/dc/terms/")
@@ -24,37 +23,43 @@ FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 DASH = Namespace("http://datashapes.org/dash#")
 
 
-#class Date(Schema):
-#    value = fields.DateTime(predicate=RDF.value)
-#    type = fields.
-
 class RDFBaseModel(BaseModel):
-    def rdf(self, graph, subject=BNode()):
-        for f in self.__fields__.values():
+
+    rdf_subject: Literal = Field(default_factory=BNode)
+    rdf_type: URIRef = None
+
+    @classmethod
+    def _rdf_fields(cls):
+        for f in cls.__fields__.values():
+            if f.alias not in ['rdf_subject', 'rdf_type']:
+                yield f
+
+    def rdf(self, graph):
+        for f in self._rdf_fields():
             predicate = f.field_info.extra['rdf_predicate']
-            predicate = URIRef(predicate)
             values = getattr(self, f.name, Undefined)
             if values is not Undefined:
                 if not isinstance(values, list):
                     values = [values]
                 for value in values:
                     if isinstance(value, RDFBaseModel):
-                        sub = BNode()
-                        graph.add((subject, predicate, sub))
-                        graph = value.rdf(graph, sub)
+                        graph.add((self.rdf_subject, predicate, value.rdf_subject))
+                        graph = value.rdf(graph)
                     else:
                         value = Literal(value)
-                        graph.add((subject, predicate, value))
+                        graph.add((self.rdf_subject, predicate, value))
+        if self.rdf_type:
+            graph.add((self.rdf_subject, RDF.type, self.rdf_type))
         return graph
 
-    def rdf_string(self, subject=BNode(), rdf_format='ttl'):
+    def rdf_string(self, rdf_format='ttl'):
         g = Graph()
-        return self.rdf(g, subject).serialize(format=rdf_format).decode()
+        return self.rdf(g).serialize(format=rdf_format).decode()
 
     @classmethod
     def parse(cls, metadata_graph, subject):
-        kwargs = {}
-        for f in cls.__fields__.values():
+        kwargs = {'rdf_subject': subject}
+        for f in cls._rdf_fields():
             predicate = f.field_info.extra['rdf_predicate']
             parsed = []
             for value in metadata_graph.objects(subject=subject, predicate=predicate):
@@ -66,10 +71,11 @@ class RDFBaseModel(BaseModel):
                     parsed.append(clazz.parse(metadata_graph, value))
                 else:
                     parsed.append(value.value)
-            if f.sub_fields:
-                kwargs[f.name] = parsed
-            else:
-                kwargs[f.name] = parsed[0]
+            if len(parsed) > 0:
+                if f.sub_fields:
+                    kwargs[f.name] = parsed
+                else:
+                    kwargs[f.name] = parsed[0]
         return cls(**kwargs)
 
 
@@ -78,6 +84,7 @@ class Date(RDFBaseModel):
     value: datetime = Field(rdf_predicate=RDF.value)
 
 class HydroShareResource(RDFBaseModel):
+    rdf_type: URIRef = Field(rdf_predicate=RDF.type, const=True, default=HSTERMS.resource)
     title: str = Field(rdf_predicate=DC.title)
     description: str = Field(rdf_predicate=DC.description)
     dates: List[Date] = Field(rdf_predicate=DC.date)
@@ -85,22 +92,23 @@ class HydroShareResource(RDFBaseModel):
 
 created = Date(type='created', value=datetime.now())
 modified = Date(type='modified', value=datetime.now())
-res = HydroShareResource(title="default", description="default description", dates=[created, modified], subjects=['s1', 's2', 's3'])
+res = HydroShareResource(rdf_subject=HSRESOURCE.ab3234,
+                         title="default",
+                         description="default description",
+                         dates=[created, modified],
+                         subjects=['s1', 's2', 's3'])
 res.description = "a description"
 res.title = "a title"
 res.dates[0].value = datetime.now()
+print(res.rdf_string('turtle'))
 
+g = res.rdf(Graph())
+for t in g.triples((None, None, None)):
+    print(t)
 
-g = res.rdf(Graph(), HSRESOURCE.ab3234)
-
-new_res = HydroShareResource.parse(g, HSRESOURCE.ab3234)
+new_res = HydroShareResource.parse(g, res.rdf_subject)
 print(new_res.title)
-print(new_res.dates[0].value)
+print(new_res.rdf_string('turtle'))
 
-print(res.rdf_string(HSRESOURCE.ab3234, 'turtle'))
-
-#res.parse(g, subject)
-#print(res.description)
-#print(res.title)
 
 
